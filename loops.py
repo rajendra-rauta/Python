6364c30f176d81868705a8fc6f904042232061c1
Rajendra <rajendrarauta979@gmail.com>
2024-10-05 20:13:28 +0530

    loop

--- loops.py
+++ loops.py
@@ -2,8 +2,14 @@
 #     print(hello)
 # }
 
-count =1;
-while count<=5 :
-    print("hello")
-    count +=1;
-print(count)
+# i =100;
+# while i>=1 :
+#     print(i)
+#     i -=1;
+
+
+n= 2 
+i=1;
+while i<=10 :
+    print(n *i )
+    i +=1
