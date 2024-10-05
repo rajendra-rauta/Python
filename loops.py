2ebf13ec2bb2be3c188ded2cf41c0ee463434c8d
Rajendra <rajendrarauta979@gmail.com>
2024-10-05 23:25:39 +0530

    loop

--- loops.py
+++ loops.py
@@ -8,8 +8,26 @@
 #     i -=1;
 
 
-n= 2 
-i=1;
-while i<=10 :
-    print(n *i )
-    i +=1
+# n= 2 
+# i=1;
+# while i<=10 :
+#     print(n *i )
+#     i +=1
+
+# nums= (1 ,2,3,4,5,6,7,8,9)
+
+# x=5
+# i=0
+# while i<len(nums):
+#     if(nums[i]==x):
+#         print("Found at idx", i)
+# else :
+#     print("finding...")
+#     i+=1
+
+
+
+nums= (1 ,2,3,4,5,6,7,8,9)
+
+for num in nums :
+    print(num)
