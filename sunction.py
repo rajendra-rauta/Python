f0630fd589b695fb137905757de05af16a9ab8c1
Rajendra <rajendrarauta979@gmail.com>
2024-10-06 11:14:33 +0530

    function

--- sunction.py
+++ sunction.py
@@ -1,5 +1,13 @@
-def cal_sum(a,b) :
-    sum = a+b
-    print(sum)
-    return sum 
+# def cal_sum(a,b) :
+#     sum = a+b
+#     print(sum)
+#     return sum 
 
+
+def converter(usd_val):
+    inr_val = usd_val * 83
+    print(usd_val,"USD =" ,inr_val, "INR")
+
+
+
+converter(20)
