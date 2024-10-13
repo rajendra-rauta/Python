75dfe7dbe9f9d93f2bbad4d7e6247b28903bfbfd
Rajendra <rajendrarauta979@gmail.com>
2024-10-13 08:12:55 +0530

    funk

--- map.py
+++ map.py
@@ -22,7 +22,7 @@ waves = np.random.rand(grid_size, grid_size)  # Random wave costs
 currents = np.random.rand(grid_size, grid_size)  # Random current costs
 
 # Set costs in the grid
-for y in range(grid_size):
+for y in range(grid_size):  
     for x in range(grid_size):
         set_cell_cost(grid, x, y, wind, waves, currents)
 
