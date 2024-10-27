407e74833da8ad01ef90b0b04006a403f50635a1
Rajendra <rajendrarauta979@gmail.com>
2024-10-27 09:47:24 +0530

    file

--- path.py
+++ path.py
@@ -44,7 +44,7 @@ def a_star(grid, start, goal):
 # Visualization function with standard colors
 def visualize_path(grid, path, start, goal):
     grid = np.array(grid)
-    for (x, y) in path: 
+    for (x, y) in path:  
         grid[x, y] = 2  # Mark the path
     grid[start[1], start[1]] = 3 # Mark the start
     grid[goal[0], goal[1]] = 4  # Mark the goal
