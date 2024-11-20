ddd1756930303d9f3eea0d08340b3fc27bf04ad7
Rajendra <rajendrarauta979@gmail.com>
2024-11-20 08:39:04 +0530

    example added

--- assignment.py
+++ assignment.py
@@ -27,3 +27,17 @@ while True:
 if largest is not None and smallest is not None:
     print("Maximum is", largest)
     print("Minimum is", smallest)
+
+
+
+
+
+Enter a number (or 'done' to finish): 7
+Enter a number (or 'done' to finish): 2
+Enter a number (or 'done' to finish): bob
+Invalid input
+Enter a number (or 'done' to finish): 10
+Enter a number (or 'done' to finish): 4
+Enter a number (or 'done' to finish): done
+Maximum is 10
+Minimum is 2
